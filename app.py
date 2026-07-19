3bdb2fdae80daca1f4970e67aeba40e39ea3ee82
ramaadv2006 <ramaadv2006@gmail.com>
2026-07-19 17:23:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -299,7 +299,7 @@ def delete_case(case_id):
     flash("Case removed.", "success")
     return redirect(url_for("dashboard"))
 
-
+init_db()
 if __name__ == "__main__":
-    init_db()
+    
     app.run(debug=True, host="0.0.0.0", port=5000)
